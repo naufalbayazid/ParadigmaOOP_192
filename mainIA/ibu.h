3987c8a5c71427ec253a221f1e2704708743f504
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-05-22 15:52:57 +0700

    daftar anak dari ibu

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -18,3 +18,12 @@ public:
 }; 
 void ibu::tambahAnak(anak* pAnak) {
 	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
