5ec45f479a35bc5aae6fd099d04803cbb295d4f9
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-05-22 18:29:53 +0700

    jantung di idupkan

---
+++ ParadigmaOOP_192/jantung.h
@@ -0,0 +1,19 @@
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+#include <iostream>
+#include "jantung.h"
+#include <vector>
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif
+
+
+
